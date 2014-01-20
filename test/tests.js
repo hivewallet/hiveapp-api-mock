3243f16b99955c1f95e5416b665eba54f97cd5ce
Wei Lu <luwei.here@gmail.com>
2014-01-20 20:14:56 +0800

    add tests for getUserInfo

--- test/tests.js
+++ test/tests.js
@@ -3,3 +3,12 @@ describe('constants', function(){
   it('MBTC_IN_SATOSHI', function(){ expect(bitcoin.MBTC_IN_SATOSHI).to.eql(100000) })
   it('UBTC_IN_SATOSHI', function(){ expect(bitcoin.UBTC_IN_SATOSHI).to.eql(100) })
 })
+
+describe('getUserInfo', function(){
+  it('passes user info hash to callback provided', function(){
+    bitcoin.getUserInfo(function(info){
+      expect(info).to.only.have.keys('firstName', 'lastName', 'email', 'address')
+    })
+  })
+})
+
