b62a3e4b3b6fa6e30633867bff68cf2c17459a9b
Wei Lu <luwei.here@gmail.com>
2014-01-20 20:03:27 +0800

    setup mocha for testing & examples

---
+++ test/tests.js
@@ -0,0 +1,5 @@
+describe('constants', function(){
+  it('BTC_IN_SATOSHI', function(){ expect(bitcoin.BTC_IN_SATOSHI).to.eql(100000000) })
+  it('MBTC_IN_SATOSHI', function(){ expect(bitcoin.MBTC_IN_SATOSHI).to.eql(100000) })
+  it('UBTC_IN_SATOSHI', function(){ expect(bitcoin.UBTC_IN_SATOSHI).to.eql(100) })
+})
